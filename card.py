94fc3fe58f86a44e2c720b2ad5810a9e64b62d06
Mr. Devet <casey.devet@sccdsb.net>
2023-11-22 06:44:59 -0500

    initial commit

---
+++ card.py
@@ -0,0 +1,6 @@
+# Programmer: 
+# Description: 
+
+VALID_VALUES = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
+VALID_SUITS = ["Clubs", "Diamonds", "Spades", "Hearts"]
+
